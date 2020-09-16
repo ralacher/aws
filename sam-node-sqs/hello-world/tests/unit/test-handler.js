260441f070fd8ea731f51929b73a68b362361139
Robert Lacher <robert.lacher@gmail.com>
2020-09-16 08:46:21 -0400

    Updated unit tests to pass

--- sam-node-sqs/hello-world/tests/unit/test-handler.js
+++ sam-node-sqs/hello-world/tests/unit/test-handler.js
@@ -1,22 +1,24 @@
 'use strict';
 
 const app = require('../../app.js');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const event = require('../../../events/event.json')
 const chai = require('chai');
 const expect = chai.expect;
-var event, context;
+var context;
 
 describe('Tests index', function () {
-    it('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
+    it('verifies count of S3 objects', async () => {
+        await app.lambdaHandler(event, context);
 
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
+        const objects = [];
+        const result = await s3.listObjects({
+            Bucket: "rlacher"
+        }).promise();
+        result.Contents.forEach(data => {
+            objects.push(data.Key);
+        });
+        expect(objects).to.have.lengthOf(2);
     });
 });
